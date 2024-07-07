b9a3d2fb3cfecea7c0d6d1ae70d04becf9dfde63
Doniyor <dbek59141@gmail.com>
2024-07-07 02:57:56 -0700

    routes file and struct finshied

---
+++ src/constans/path.js
@@ -0,0 +1,16 @@
+// auth 
+
+export const authRoutes = {
+
+}
+
+// public...
+
+export const publicRoutes = {
+    home: "/",
+    discover: "/discover",
+    products: "/products",
+    authors: "/authors",
+    feed: "/feed",
+    contact: "/contact"
+}
