f762ae7053d4d6130cdf640a5c50233ff003045a
Doniyor <dbek59141@gmail.com>
2024-07-10 22:25:58 -0700

    main Header finsih

--- src/components/main/header/mainHeader.jsx
+++ src/components/main/header/mainHeader.jsx
@@ -2,26 +2,57 @@ import styles from "./MainHeader.module.css";
 import Logo from "../../../assets/Icon/Logo.svg";
 import { Link } from "react-router-dom";
 import SearchInput from "../../searchBox/SearchInput";
+import { IconButton } from "@mui/material";
+import {
+  FavoriteBorderOutlined,
+  PermIdentityOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+import { useCallback, useState } from "react";
 
 const MainHeader = ({ data }) => {
+  // state
+  const [searchVal, setSearchVal] = useState("");
+
+  // Handle Search value
+  const handleChangeSearch = useCallback((val) => {
+    setSearchVal(val);
+  }, []);
+
   return (
     <div className={styles.header}>
-      {/* Logo */}
-      <div className={styles.mainLogo}>
-        <img className={styles.logo} src={Logo} alt="Logo" />
-      </div>
-      {/* List */}
-      <ul className={styles.lists}>
-        {data.map((item) => (
-          <li className={styles.listItem} key={item.key}>
-            <Link className={styles.listItemLink} to={item.to}>
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className={styles.body}>
+        {/* Logo */}
+        <div className={styles.mainLogo}>
+          <img className={styles.logo} src={Logo} alt="Logo" />
+        </div>
+        {/* List */}
+        <ul className={styles.lists}>
+          {data.map((item) => (
+            <li className={styles.listItem} key={item.key}>
+              <Link className={styles.listItemLink} to={item.to}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
 
-      <SearchInput />
+        {/* Search Input */}
+        <SearchInput value={searchVal} setValue={handleChangeSearch} />
+
+        {/* 3 btn */}
+        <div>
+          <IconButton aria-label="Favourites">
+            <FavoriteBorderOutlined />
+          </IconButton>
+          <IconButton aria-label="Favourites">
+            <PermIdentityOutlined />
+          </IconButton>
+          <IconButton aria-label="Favourites">
+            <ShoppingCartOutlined />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/searchBox/SearchInput.jsx
+++ src/components/searchBox/SearchInput.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { InputAdornment, TextField } from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import styles from "./Input.module.css"
+import { memo } from "react";
 
-const SearchInput = () => {
+const SearchInput = ({value,setValue}) => {
   return (
-    <form>
-        <input type="text" placeholder='Search'/>
-    </form>
-  )
-}
+    <TextField
+      className={styles.inpt}
+      placeholder="Search"
+      value={value}
+      onChange={(e)=>setValue(e.target.value)}
+      size="small"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchOutlinedIcon />
+          </InputAdornment>
+        ),
+      }}
+      variant="outlined"
+    />
+  );
+};
 
-export default SearchInput
+export default memo(SearchInput);
